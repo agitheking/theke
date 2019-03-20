5144ba9599689b8e91188be0d22cd4ab515685e9
artur <artur@localhost.localdomain>
2019-03-20 22:07:02 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShiftsComponent } from './shifts/shifts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShiftsComponent
   ],
   imports: [
     BrowserModule,
