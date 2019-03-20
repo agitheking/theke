7de3f2120143da7e5ed7c8cd715b310475488f4c
artur <artur@localhost.localdomain>
2019-03-20 23:45:23 +0100

    add message c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShiftsComponent } from './shifts/shifts.component';
 import { ShiftDetailComponent } from './shift-detail/shift-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShiftsComponent,
-    ShiftDetailComponent
+    ShiftDetailComponent,
+    MessagesComponent
     
   ],
   imports: [
